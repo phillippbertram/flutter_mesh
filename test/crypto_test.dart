ff7ff6461a86b52d12fb1ba5870ef2e3f4221967
Phillipp Bertram <me@phillippbertram.de>
2024-05-05 19:00:00 +0200

    add crypto tests

--- test/crypto_test.dart
+++ test/crypto_test.dart
@@ -4,8 +4,12 @@
 // writing unit tests, visit
 // https://flutter.dev/docs/cookbook/testing/unit/introduction
 
+import 'package:flutter_mesh/src/mesh/mesh.dart';
+import 'package:flutter_mesh/src/mesh/type_extensions/data.dart';
 import 'package:flutter_mesh/src/mesh/utils/crypto.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+// TODO: write tests
 
 void main() {
   group('Crypto', () {
@@ -15,12 +19,37 @@ void main() {
         final rand1_2 = Crypto.generateRandomBytes(1);
         final rand2 = Crypto.generateRandomBytes(2);
         final rand16 = Crypto.generateRandomBytes(16);
-
         expect(rand1.length, 1);
         expect(rand1_2.length, 1);
         expect(rand1, isNot(rand1_2));
         expect(rand2.length, 2);
         expect(rand16.length, 16);
+      });
+
+      test('generateRandomBits', () {
+        final rand16 = Crypto.generateRandomBits(128);
+        final rand16_2 = Crypto.generateRandomBits(128);
+        final rand32 = Crypto.generateRandomBits(256);
+        expect(rand16.length, 16);
+        expect(rand16_2.length, 16);
+        expect(rand16, isNot(rand16_2));
+        expect(rand32.length, 32);
+      });
+
+      test('generateRandom128BitKey', () {
+        final key1 = Crypto.generateRandom128BitKey();
+        final key2 = Crypto.generateRandom128BitKey();
+        expect(key1.length, 16);
+        expect(key2.length, 16);
+        expect(key1, isNot(key2));
+      });
+
+      test('generateRandom256BitKey', () {
+        final key1 = Crypto.generateRandom256BitKey();
+        final key2 = Crypto.generateRandom256BitKey();
+        expect(key1.length, 32);
+        expect(key2.length, 32);
+        expect(key1, isNot(key2));
       });
     });
 
@@ -33,6 +62,24 @@ void main() {
       //   print(keyPair);
       //   expect(keyPair, isNotNull);
       // });
+    });
+
+    group("virtual label", () {
+      test("calculateVirtualLabel", () {
+        const expected = Address(0xADD5);
+        const label = "12345678-1234-1234-1234-12345678ABCD";
+        final result = Crypto.calculateVirtualAddress(label);
+        expect(result, expected);
+      });
+    });
+
+    group("Aid", () {
+      test("calculateAid", () {
+        final key = DataUtils.fromHex("3216d1509884b533248541792b877f98")!;
+        const expectedAid = 0x38;
+        final aid = Crypto.calculateAid(key);
+        expect(aid, expectedAid);
+      });
     });
   });
 }
