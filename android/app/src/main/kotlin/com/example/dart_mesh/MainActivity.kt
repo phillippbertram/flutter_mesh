843ee2965483a8a4177694c238114e75fb3eb3b0
Phillipp Bertram <me@phillippbertram.de>
2024-05-27 20:28:32 +0200

    android crypto adjustments

--- android/app/src/main/kotlin/com/example/dart_mesh/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dart_mesh/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.flutter_mesh
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import java.security.Security
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Security.insertProviderAt(org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
+}
+
